2cd8d26fc52910e263f47935ef83fcb1ce8cee57
vikrant9528 <vikrantchaudhary258@gmail.com>
2024-09-26 15:47:23 +0530

    first version of bharat clock

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import ClockHeading from './components/ClockHeading'
+import ClockSlogan from './components/ClockSlogan'
+import CurrentTime from './components/CurrentTime'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+
+
+function App(){
+  return <center>
+    <ClockHeading/>
+    <ClockSlogan/>
+    <CurrentTime/>
+  </center>
+
+ 
+}
+
+export default App
---
+++ src/components/ClockHeading.jsx
@@ -0,0 +1,4 @@
+let ClockHeading = () => {
+return <h1 className="fw-bolder">Bharat clock</h1>
+};
+export default ClockHeading;
